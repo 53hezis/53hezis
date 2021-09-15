91fd8feb3a8ce1f9e5a0e69f50efba983a2987dd
LQ <932639127@qq.com>
2021-09-15 11:13:25 +0800

    book

--- Film/routes/dist/video.dev.js
+++ Film/routes/dist/video.dev.js
@@ -4,13 +4,13 @@ var express = require('express');
 
 var pool = require('../pool.js');
 
-var n = express.Router(); //搜索电影模块
+var n = express.Router(); //搜索单个电影模块
 //页面地址：http://127.0.0.1:9999/vquery.html
 //请求方式：POST
-//路由地址：/v1/video/search  转参方式
-//这个路由接收参数:/search    -->  req.body
+//路由地址：/v1/video/uactor  转参方式
+//这个路由接收参数:/uactor    -->  req.body
 
-n.post('/search', function (req, res, next) {
+n.post('/uactor', function (req, res, next) {
   var obj = req.body;
   console.log(obj);
   pool.query('select * from wd_movie where title=?', [obj.title], function (err, data) {
@@ -19,19 +19,28 @@ n.post('/search', function (req, res, next) {
       return;
     }
 
-    res.send({
-      "code": 200,
-      "msg": '查找成功',
-      "data": data
-    });
-  });
-}); //搜索全部演员
+    console.log(data);
+
+    if (data.length === 0) {
+      res.send({
+        code: 201,
+        msg: '查找失败'
+      });
+    } else {
+      res.send({
+        code: 200,
+        msg: '查找成功',
+        data: data
+      });
+    }
+  }); //res.send('找到啦！！');
+}); //搜索全部演员******
 //页面地址：http://127.0.0.1:9999/home_page.html
 //请求方式：GET
 //路由地址：/v1/user/actor  转参方式
 //这个路由接收参数:/actor    -->  req.body
 
-n.get('/actor', function (req, res, next) {
+n.post('/actor', function (req, res, next) {
   pool.query('select * from wd_actor', function (err, data) {
     if (err) {
       next(err);
@@ -46,7 +55,12 @@ n.get('/actor', function (req, res, next) {
       "data": data
     });
   });
-}); //删除用户
+}); //管理员后台******
+//页面地址：http://127.0.0.1:9999/list.html
+//请求方式：DELETE
+//路由地址：/v1/user//del/:aid  转参方式
+//这个路由接收参数:/actor    -->  req.body
+//删除用户
 
 n["delete"]('/del/:aid', function (req, res, next) {
   var obj = req.params; //console.log(obj);
@@ -71,7 +85,7 @@ n["delete"]('/del/:aid', function (req, res, next) {
     }
   });
 }); //搜索单个演员模块
-//页面地址：http://127.0.0.1:9999/home_page.html
+//页面地址：http://127.0.0.1:9999/uquery.html
 //请求方式：post
 //路由地址：/v1/user/upname  转参方式
 //这个路由接收参数:/upname    -->  req.body
@@ -101,19 +115,23 @@ n.post('/upname', function (req, res, next) {
     }
   }); //res.send('找到啦！！');
 }); //搜索导演模块
+//页面地址：http://127.0.0.1:9999/V_director.html
+//请求方式：/udirector
+//路由地址：/v1/user/udirector  转参方式
+//这个路由接收参数:/udirector
 
-n.get('/dvideo/udirector', function (req, res, next) {
+n.post('/udirector', function (req, res, next) {
   var obj = req.body;
   console.log(obj);
-  pool.query('select * from wd_movie where director=?', [obj.director], function (err, result) {
+  pool.query('select * from wd_movie where director=?', [obj.director], function (err, data) {
     if (err) {
       next(err);
       return;
     }
 
-    console.log(result);
+    console.log(data);
 
-    if (result.length === 0) {
+    if (data.length === 0) {
       res.send({
         code: 201,
         msg: '查找失败'
@@ -122,24 +140,28 @@ n.get('/dvideo/udirector', function (req, res, next) {
       res.send({
         code: 200,
         msg: '查找成功',
-        data: result
+        data: data
       });
     }
   }); //res.send('找到啦！！');
-}); //演员相关电影
+}); //电影相关演员
+//页面地址：http://127.0.0.1:9999/V_correlation.html
+//请求方式：GEt
+//路由地址：/v1/video/related 转参方式
+//这个路由接收参数:/related
 
-n.get('/dvideo/related', function (req, res, next) {
+n.post('/related', function (req, res, next) {
   var obj = req.body;
   console.log(obj);
-  pool.query('select * from wd_actor where movie_id=?', [obj.movie_id], function (err, result) {
+  pool.query('SELECT * FROM wd_actor WHERE movie_id=?', [obj.movie_id], function (err, data) {
     if (err) {
       next(err);
       return;
     }
 
-    console.log(result);
+    console.log(data);
 
-    if (result.length === 0) {
+    if (data.length === 0) {
       res.send({
         code: 201,
         msg: '查找失败'
@@ -148,7 +170,7 @@ n.get('/dvideo/related', function (req, res, next) {
       res.send({
         code: 200,
         msg: '查找成功',
-        data: result
+        data: data
       });
     }
   }); //res.send('找到啦！！');
--- Film/routes/video.js
+++ Film/routes/video.js
@@ -1,131 +1,141 @@
-const express=require('express');
-const pool=require('../pool.js');
-const n=express.Router();
+const express = require('express');
+const pool = require('../pool.js');
+const n = express.Router();
 
-//搜索电影模块
-   //页面地址：http://127.0.0.1:9999/vquery.html
-   //请求方式：POST
-   //路由地址：/v1/video/search  转参方式
-   //这个路由接收参数:/search    -->  req.body
-n.post('/search',(req,res,next)=>{
-    let obj=req.body;
-	console.log(obj);
- pool.query('select * from wd_movie where title=?',[obj.title],(err,data)=>{
-   if (err){
-       next(err);
+//搜索单个电影模块
+//页面地址：http://127.0.0.1:9999/vquery.html
+//请求方式：POST
+//路由地址：/v1/video/uactor  转参方式
+//这个路由接收参数:/uactor    -->  req.body
+n.post('/uactor', (req, res, next) => {
+  let obj = req.body;
+  console.log(obj);
+  pool.query('select * from wd_movie where title=?', [obj.title], (err, data) => {
+    if (err) {
+      next(err);
       return;
-   }
-       res.send({"code":200,"msg":'查找成功',"data":data});
- });
+    }
+    console.log(data);
+    if (data.length === 0) {
+      res.send({ code: 201, msg: '查找失败' });
+    } else {
+      res.send({ code: 200, msg: '查找成功', data: data });
+    }
+  });
+  //res.send('找到啦！！');
 });
 
-
-
-
-
-
-
-
-//搜索全部演员
-   //页面地址：http://127.0.0.1:9999/home_page.html
-   //请求方式：GET
-   //路由地址：/v1/user/actor  转参方式
-   //这个路由接收参数:/actor    -->  req.body
-n.get('/actor',(req,res,next)=>{
-  pool.query('select * from wd_actor',(err,data)=>{
-    if(err){
+//搜索全部演员******
+//页面地址：http://127.0.0.1:9999/home_page.html
+//请求方式：GET
+//路由地址：/v1/user/actor  转参方式
+//这个路由接收参数:/actor    -->  req.body
+n.post('/actor', (req, res, next) => {
+  pool.query('select * from wd_actor', (err, data) => {
+    if (err) {
       next(err);
       return;
     }
     //查看数据库返回的数据
     //console.log(data);
-    res.send({"code":1,"msg":"找到所有用户","data":data})
+    res.send({ "code": 1, "msg": "找到所有用户", "data": data })
   });
-  });
+});
 
+
+//管理员后台******
+//页面地址：http://127.0.0.1:9999/list.html
+//请求方式：DELETE
+//路由地址：/v1/user//del/:aid  转参方式
+//这个路由接收参数:/actor    -->  req.body
 //删除用户
-  n.delete('/del/:aid',(req,res,next)=>{
-    let obj = req.params;
-    //console.log(obj);
-    pool.query('delete from wd_actor where id=?',[obj.id],(err,data)=>{
-      if(err){
-        next(err);
-        return;
-      }
-      //console.log(data.affectedRows);
-      if(data.affectedRows===0){
-        res.send({"code":0,"msg":"删除失败"});
-      }else{
-        res.send({"code":1,"msg":"删除成功"});
-      }
-    });
+n.delete('/del/:aid', (req, res, next) => {
+  let obj = req.params;
+  //console.log(obj);
+  pool.query('delete from wd_actor where id=?', [obj.id], (err, data) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    //console.log(data.affectedRows);
+    if (data.affectedRows === 0) {
+      res.send({ "code": 0, "msg": "删除失败" });
+    } else {
+      res.send({ "code": 1, "msg": "删除成功" });
+    }
   });
-
-
-
+});
 
 //搜索单个演员模块
-   //页面地址：http://127.0.0.1:9999/home_page.html
-   //请求方式：post
-   //路由地址：/v1/user/upname  转参方式
-   //这个路由接收参数:/upname    -->  req.body
-n.post('/upname',(req,res,next)=>{
-    let obj=req.body;
-	console.log(obj);
-  pool.query('select * from wd_actor where pname=?',[obj.pname],(err,data)=>{
-     if (err){
-        next(err);
-       return;
-     }
-        console.log(data);
-     if (data.length===0){
-       res.send({code:201,msg:'查找失败'});
-     }else{
-       res.send({code:200,msg:'查找成功',data:data});
-     }
-   });
-    //res.send('找到啦！！');
+//页面地址：http://127.0.0.1:9999/uquery.html
+//请求方式：post
+//路由地址：/v1/user/upname  转参方式
+//这个路由接收参数:/upname    -->  req.body
+n.post('/upname', (req, res, next) => {
+  let obj = req.body;
+  console.log(obj);
+  pool.query('select * from wd_actor where pname=?', [obj.pname], (err, data) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    console.log(data);
+    if (data.length === 0) {
+      res.send({ code: 201, msg: '查找失败' });
+    } else {
+      res.send({ code: 200, msg: '查找成功', data: data });
+    }
   });
+  //res.send('找到啦！！');
+});
 
 
 //搜索导演模块
-n.get('/dvideo/udirector',(req,res,next)=>{
-    let obj=req.body;
-	console.log(obj);
-  pool.query('select * from wd_movie where director=?',[obj.director],(err,result)=>{
-    if (err){
-        next(err);
-        return;
-       }
-        console.log(result);
-    if (result.length===0){
-        res.send({code:201,msg:'查找失败'});
-    }else{
-        res.send({code:200,msg:'查找成功',data:result});
+//页面地址：http://127.0.0.1:9999/V_director.html
+//请求方式：/udirector
+//路由地址：/v1/user/udirector  转参方式
+//这个路由接收参数:/udirector
+n.post('/udirector', (req, res, next) => {
+  let obj = req.body;
+  console.log(obj);
+  pool.query('select * from wd_movie where director=?', [obj.director], (err, data) => {
+    if (err) {
+      next(err);
+      return;
     }
-    });
-    //res.send('找到啦！！');
+    console.log(data);
+    if (data.length === 0) {
+      res.send({ code: 201, msg: '查找失败' });
+    } else {
+      res.send({ code: 200, msg: '查找成功', data: data });
+    }
   });
+  //res.send('找到啦！！');
+});
 
 
-//演员相关电影
-n.get('/dvideo/related',(req,res,next)=>{
-    let obj=req.body;
-	console.log(obj);
-  pool.query('select * from wd_actor where movie_id=?',[obj.movie_id],(err,result)=>{
-    if (err){
-        next(err);
-        return;
-       }
-        console.log(result);
-    if (result.length===0){
-        res.send({code:201,msg:'查找失败'});
-    }else{
-        res.send({code:200,msg:'查找成功',data:result});
+//电影相关演员
+//页面地址：http://127.0.0.1:9999/V_correlation.html
+//请求方式：GEt
+//路由地址：/v1/video/related 转参方式
+//这个路由接收参数:/related
+n.post('/related', (req, res, next) => {
+  let obj = req.body;
+  console.log(obj);
+  pool.query('SELECT * FROM wd_actor WHERE movie_id=?', [obj.movie_id], (err, data) => {
+    if (err) {
+      next(err);
+      return;
     }
-    });
-    //res.send('找到啦！！');
+    console.log(data);
+    if (data.length === 0) {
+      res.send({ code: 201, msg: '查找失败' });
+    } else {
+      res.send({ code: 200, msg: '查找成功', data: data });
+    }
   });
+  //res.send('找到啦！！');
+});
 
 //导出模块
-module.exports=n;
+module.exports = n;
